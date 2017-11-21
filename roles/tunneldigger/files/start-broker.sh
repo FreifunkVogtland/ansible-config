ad3254fce4d200d6b04e05490de5794ffb2ab781
Sven Eckelmann <sven@narfation.org>
2017-11-21 15:48:49 +0100

    Add hack to switch to original tunneldigger feed

--- roles/tunneldigger/files/start-broker.sh
+++ roles/tunneldigger/files/start-broker.sh
@@ -6,4 +6,4 @@ VIRTUALENV_DIR=/opt/freifunk/tunneldigger
 cd $WDIR
 source $VIRTUALENV_DIR/bin/activate
 
-bin/python broker/l2tp_broker.py broker/l2tp_broker.cfg
+bin/python -m tunneldigger_broker.main broker/l2tp_broker.cfg
