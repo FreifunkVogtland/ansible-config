fbae8ebf216a20f227f2385a6c955b96782b7f92
Sven Eckelmann <sven@narfation.org>
2018-07-27 19:12:17 +0200

    Stop grafana before dropping dashboard history

--- roles/grafana/files/dashboard_history_drop.sh
+++ roles/grafana/files/dashboard_history_drop.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
 
 # prevent that the grafana.db uses all the available space
+/etc/init.d/grafana-server stop
 echo 'delete from dashboard_version;vacuum;'|sqlite3 /var/lib/grafana/grafana.db
+/etc/init.d/grafana-server start
