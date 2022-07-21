FreifunkVogtland/ansible-config

commit 62287912c535af791c956be12fe394c9c2410aaa

Fix ansible python interpreter in Vagrant
Signed-off-by: Sven Eckelmann <sven@narfation.org>

Vagrantfile
@@ -23,7 +23,7 @@ Vagrant.configure(2) do |config|
 
     ansible.playbook = "site.yml"
     ansible.extra_vars = {
-        ansible_python_interpreter: "/usr/bin/env python2",
+        ansible_python_interpreter: "/usr/bin/env python3",
         gateway_if: "ens5",
     }
     ansible.skip_tags=["icvpn", "ffmap-backup"]

commit 2b7d20db6dc69c7c793a9f11c97ff055e3ad1561

Also install python3 for Vagrant
Fixes: 62287912c535 ("Fix ansible python interpreter in Vagrant")
Signed-off-by: Sven Eckelmann <sven@narfation.org>

Vagrantfile
@@ -10,7 +10,7 @@
 # you're doing.
 Vagrant.configure(2) do |config|
   config.vm.provision "shell",
-    inline: "apt-get install sudo python"
+    inline: "apt-get install sudo python3"
 
   config.vm.provision "ansible" do |ansible|
     ansible.groups = {